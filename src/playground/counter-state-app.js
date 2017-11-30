57b290cd271aa8ff3629adf59cbe6c3b431652a8
Matt Warman <warmanmr@gmail.com>
2017-11-30 08:31:30 -0500

    Update Counter App to use Component State

---
+++ src/playground/counter-state-app.js
@@ -0,0 +1,45 @@
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.count = 0;
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleSubtractOne = this.handleSubtractOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
+    };
+  }
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+  }
+  handleSubtractOne(prevState) {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleSubtractOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
