f4b9a8cbdeb1d347b0086744b5f3e8a4601ff4e4
Matt Warman <warmanmr@gmail.com>
2017-12-01 06:04:36 -0500

    Convert Header, Action, Options, and Option to Stateless Functional Components.

--- src/app.js
+++ src/app.js
@@ -56,60 +56,45 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button
+        onClick={props.handlePickOption}
+        disabled={!props.hasOptions}>
+        What should I do?
+      </button>
+    </div>
+  );
+};
 
-}
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {
+        props.options.map((option) => <Option key={option} option={option} />)
+      }
+    </div>
+  );
+};
 
-class Action extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.props.handlePickOption}
-          disabled={!this.props.hasOptions}>
-          What should I do?
-        </button>
-      </div>
-    );
-  }
-
-}
-
-class Options extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        {
-          this.props.options.map((option) => <Option key={option} option={option} />)
-        }
-      </div>
-    );
-  }
-
-}
-
-class Option extends React.Component {
-
-  render() {
-    return (
-      <div>
-        {this.props.option}
-      </div>
-    );
-  }
-}
+const Option = (props) => {
+  return (
+    <div>
+      {props.option}
+    </div>
+  );
+};
 
 class AddOption extends React.Component {
 
@@ -146,5 +131,14 @@ class AddOption extends React.Component {
   }
 
 }
+//
+// const User = (props) => {
+//   return (
+//     <div>
+//       <p>Name: {props.name}</p>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   );
+// };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
