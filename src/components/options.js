c971e5eb906f21129a59697aa38e6a7a1f72d7c9
Matt Warman <warmanmr@gmail.com>
2017-12-02 06:59:52 -0500

    Use implicit returns for stateless component functions.

--- src/components/options.js
+++ src/components/options.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 import Option from './option';
 
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
-      {
-        props.options.map((option) => (
-          <Option
-            key={option}
-            option={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {
+      props.options.map((option) => (
+        <Option
+          key={option}
+          option={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
 
 export default Options;
