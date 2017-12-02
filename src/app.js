3d9e889c24c658c0255bbb4c6c164a0c338bbd6c
Matt Warman <warmanmr@gmail.com>
2017-12-02 08:03:03 -0500

    Normalize CSS. Add base and header component styles.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/indecision-app';
 
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
